27fe293eafda078da75f7229373cd32af164b0b9
DoublePain <Doublepain547@gmail.com>
2026-01-31 17:39:02 -0800

    Change Vision subsystem to fix SIM

--- src/main/java/frc/robot/subsystems/CameraSubsystem.java
+++ src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -2,27 +2,36 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CameraSubsystem extends SubsystemBase {
 
-  private UsbCamera camera;
+    private UsbCamera camera;
 
-  public CameraSubsystem() {
-    // Start automatic capture from USB camera 0 AKA Logitech Camera
-    camera = CameraServer.startAutomaticCapture(0);
+    public CameraSubsystem() {
+        if (RobotBase.isSimulation()) {
+            // In simulation, start a dummy camera feed
+            camera = CameraServer.startAutomaticCapture();
+            // Don't set properties that don't exist in sim
+        } else {
+            // On real robot, initialize camera properly
+            camera = CameraServer.startAutomaticCapture(0);
 
+            // Wrap property settings in try/catch to avoid crashing if something fails
+            try {
+                camera.setResolution(320, 240);
+                camera.setFPS(20);
+                camera.setBrightness(50);
+                camera.setExposureAuto();
+            } catch (Exception e) {
+                System.out.println("Camera property setup failed: " + e.getMessage());
+            }
+        }
+    }
 
-    camera.setResolution(320, 240);
-    camera.setFPS(20);
-
-
-    camera.setBrightness(50);
-    camera.setExposureAuto();
-  }
-
-  @Override
-  public void periodic() {
-   
-  }
+    @Override
+    public void periodic() {
+        // Nothing needed for now
+    }
 }
